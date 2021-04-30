d7f9685031725958a436b0dbc18fc7fe5ba16cc4
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-04-30 13:29:03 +0200

    I removed existing project and created pokemon list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World');
+//array to hold pokemon list
+let pokemonList = [{name: "Pikachu", height: 0.4, types: ["electric"]},
+                   {name: "Ponyta", height: 1, types: ["fire"]},
+                   {name: "Staryu", height: 0.8, types: ["water"]}];
 
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+console.log(pokemonList);
