4af2d5a7bc229047e9abb526b4c99fd4df026a80
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-05-03 16:55:12 +0200

    I modified 'for loop' in scrip.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 //array to hold pokemon list
-let pokemonList = [{name: "Pikachu", height: 0.4, types: ["electric"]},
-                   {name: "Ponyta", height: 1, types: ["fire"]},
-                   {name: "Staryu", height: 0.8, types: ["water"]}];
+let repository = [
+  {
+  name: "Pikachu",
+  height: 0.4,
+  types: ["electric"]
+},
+  {
+    name: "Ponyta",
+    height: 1,
+    types: ["fire"]
+  },
+  {
+    name:
+    "Staryu",
+    height: 0.8,
+    types: ["water"]
+  }
+];
 
-console.log(pokemonList);
+for (let i=0; i < repository.lenght; i++){
+  if (repository[i].height <1 && repository[i].height> 0.8){
+    console.log(repository[i].name + "is a big pokemon");
+  }else if (repository[i].height < 0.8){
+    console.log(repository[i].name + "is a small pokemon");
+  }else {
+    console.log(repository[i].name + "is a avarage pokemon");
+  }
+}
