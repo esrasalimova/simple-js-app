8d5935331423ca04914df2cc3e879f2c399b008e
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-05-06 14:49:42 +0200

    I managed "for loop" with "for each loop in the file. Also created new functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //array to hold pokemon list
-let repository = [
+let pokemonRepository = (function () [
+  let pokemonList = [
   {
   name: "Pikachu",
   height: 0.4,
@@ -18,12 +19,34 @@ let repository = [
   }
 ];
 
-for (let i=0; i < repository.length; i++){
-  if (repository[i].height <1 && repository[i].height> 0.8){
-    document.write(repository[i].name + "is a big pokemon <br>");
-  }else if (repository[i].height < 0.8){
-    document.write(repository[i].name + "is a small pokemon <br>");
-  }else {
-    document.write(repository[i].name + "is a avarage pokemon <br>");
-  }
+//for (let i=0; i < repository.length; i++){
+//  if (repository[i].height <1 && repository[i].height> 0.8){
+//    document.write(repository[i].name + "is a big pokemon <br>");
+//  }else if (repository[i].height < 0.8){
+//    document.write(repository[i].name + "is a small pokemon <br>");
+//  }else {
+//    document.write(repository[i].name + "is a avarage pokemon <br>");
+//  }
+//}
+
+pokemonList.forEach(function(pokemon) => {
+  console.log(pokemon.name + 'has' + pokemon.types + 'power');
+
+});
+
+
+function getAll() {
+  return pokemonList;
 }
+
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
+};
+})();
+
+console.log(pokemonRepository.getAll() );
